df4fc6ef0262da998c64aeb13feb84bc08676f89
Explicitname <explicit6911@gmail.com>
2014-11-14 09:10:23 -0500

    First Hello World Script

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 14, 2014
+ *      Author: explicitname
+ */
+#include <iostream>
+using namespace std;
+
+int main () {
+	// Say Hello World
+	cout << "Hellow World!" << endl;
+	return 0;
+}
