c4d2ae85b643591e036e89998359b592cd8d121a
Explicitname <explicit6911@gmail.com>
2014-11-25 13:18:41 -0500

    broken math

--- Problem4.cpp
+++ Problem4.cpp
@@ -16,22 +16,36 @@ using namespace std;
 int main()
 
 {
-	int prod = 1001;
+	int prod = 1234;
 	string stringConv;
 	ostringstream convert;
-	convert << prod;
-	stringConv = convert.str();
 
 
+	for (int i = 999; i >=100; i--)
+	{
+		for (int j = 999; j >= 100; i--)
+		{
+			prod = (j*i);
+			cout << prod << endl;
 
+			convert << prod;
+			stringConv = convert.str();
+			if (stringConv == string(stringConv.rbegin(), stringConv.rend()))
+				{
+				    cout << stringConv << " is a palindrome";
+				}
 
-
-
-	cout << stringConv << endl;
-
-	if (stringConv == string(stringConv.rbegin(), stringConv.rend())) {
-	    cout << stringConv << " is a palindrome";
+		}
 	}
+
+
+
+
+
+
+
+
+
 
 
 
