37630339c90ce76bec75a5a95163f74318940274
Explicitname <explicit6911@gmail.com>
2014-11-25 13:57:09 -0500

    Palindrome checking proof

--- Problem4.cpp
+++ Problem4.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <cmath>
 
 using namespace std;
 
@@ -16,23 +17,27 @@ using namespace std;
 int main()
 
 {
-	int prod = 1234;
+	int prod = 100001;
 	string stringConv;
 	ostringstream convert;
 
 
 	for (int i = 999; i >=100; i--)
 	{
-		for (int j = 999; j >= 100; i--)
+		for (int j = 999; j >= 100; j--)
 		{
-			prod = (j*i);
-			cout << prod << endl;
+			//prod = (j*i);
+			//cout << prod << endl;
+			//cout << i << " x " << j << "\n" << endl;
+
 
 			convert << prod;
 			stringConv = convert.str();
 			if (stringConv == string(stringConv.rbegin(), stringConv.rend()))
 				{
-				    cout << stringConv << " is a palindrome";
+				    cout << stringConv << " is a palindrome. " << endl;
+
+				    return 0;
 				}
 
 		}
@@ -52,6 +57,7 @@ int main()
 
 
 
+	return 0;
 
 
 }
