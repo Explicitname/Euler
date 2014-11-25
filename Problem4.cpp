4e09e36f83849ca2c8279053c61313cf94a418b6
Explicitname <explicit6911@gmail.com>
2014-11-24 19:01:49 -0500

    Figured out sstream conversions and palindrome checking

--- Problem4.cpp
+++ Problem4.cpp
@@ -7,6 +7,8 @@
 
 
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -14,25 +16,28 @@ using namespace std;
 int main()
 
 {
-	int prod = 0;
-	string checkStr = "";
+	int prod = 1001;
+	string stringConv;
+	ostringstream convert;
+	convert << prod;
+	stringConv = convert.str();
+
+
+
+	/*string checkStr = "";
 
 	for (int i=100; i<=999; i++)
 	{
 
 	}
+	*/
 
 
+	cout << stringConv << endl;
 
-
-
-
-
-
-
-
-
-
+	if (stringConv == string(stringConv.rbegin(), stringConv.rend())) {
+	    cout << stringConv << " is a palindrome";
+	}
 
 
 
