07c9e75411941a2dbc62346a43457ca5c2e71ac0
Explicitname <explicit6911@gmail.com>
2014-12-01 10:50:00 -0500

    Problem 5 Solution.

--- Problem5.cpp
+++ Problem5.cpp
@@ -13,21 +13,31 @@ int main()
 {
 
 	bool cont = true;
-	int i = 1;
+	int i = 0;
+
+	cout << "This program will find the smallest number that is evenly divisble by all the numbers from 1 through 20." << endl;
+	cout << "..." << endl;
+	cout << "..." << endl;
+	cout << "..." << endl;
+	cout << "..." << endl;
+	cout << "..." << endl;
+	cout << "..." << endl;
 
 	while (cont == true)
 
 	{
 
+		i++;
 
-		if (!(i % 20) && !(i % 19) && !(i % 18) && !(i % 17) && !(i % 16) && !(i % 15) && !(i % 14))
+
+		if (!(i % 20) && !(i % 19) && !(i % 18) && !(i % 17) && !(i % 16) && !(i % 15) && !(i % 14) && !(i % 13) && !(i % 12) && !(i % 11))
 		{
-			cout << i << endl;
+			cout << "This smallest number that can divided evenly by all the numbers between 1-20 is: " << i << endl;
 			cont = false;
 			return 0;
 		}
 
-		i++;
+
 
 
 
