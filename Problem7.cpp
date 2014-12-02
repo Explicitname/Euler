1afca87fddd7e55a8c8ac4f172c7b15b7cd5cdf0
Explicitname <explicit6911@gmail.com>
2014-12-02 09:57:39 -0500

    Problem on line 13. Possible typo. Loop changed.

--- Problem7.cpp
+++ Problem7.cpp
@@ -10,58 +10,38 @@
 
 using namespace std;
 
-
-
 int isPrime(int num)
 
 {
 
-	for (int i = 2; i < num; i++)
-	{
-		if ((num % i) == 0)
-		{
+	for (int i = 2; i < num; i++) {
+		if ((num % i) == 0) {
 			i = num;
 			return 0;
 
-		}
-		else
-		{
+		} else {
 			return 1;
 		}
 	}
-
 
 }
 
 int main()
 
 {
-	int num = 2;
-	vector <int> primeVect;
+	int maxVect = 10001;
+	vector<int> primeVect;
 
-	while (primeVect.size < 10001)
+	for (int num = 2; num < maxVect; num++)
 
 	{
-		if (isPrime(num) == 1)
-		{
+		if (isPrime(num) == 1) {
 			primeVect.push_back(num);
 		}
 
-
 	}
-
 	cout << primeVect[10000] << endl;
-
-
-
-
-
-
-
-
 
 	return 0;
 }
-
-
 
